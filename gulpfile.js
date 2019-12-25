9d7ba5b2a2009451a709687014467e81f6e05f9a
carvalhojg <48341861+carvalhojg@users.noreply.github.com>
2019-12-24 17:23:21 -0800

    updated files
    Changed semantics on html.
    Copy and past my gitignore for node modules.
    Add array and loop to show only 12 stories.
    Add browser-sync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,72 @@
 // Initialize modules
-const { src, dest, watch, series, parallel } = require('gulp');
-const autoprefixer = require ('autoprefixer');
-const cssnano = require ('gulp-cssnano');
-const concat = require ('gulp-concat');
-const postcss = require ('gulp-postcss');
-const replace = require ('gulp-replace');
-const sass = require ('gulp-sass');
-const sourcemaps = require ('gulp-sourcemaps');
-const terser = require ('gulp-terser');
-
-
-
+const { src, dest, watch, series, parallel, task } = require('gulp'),
+    autoprefixer = require('autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    concat = require('gulp-concat'),
+    postcss = require('gulp-postcss'),
+    replace = require('gulp-replace'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    terser = require('gulp-terser'),
+    browserSync = require('browser-sync').create(); // add browser sync to visite webpage using gulp
 
 // File path variables
 const files = {
-	scssPath: 'app/scss/**/*.scss' ,
-	jsPath: 'app/js/**/*.js'
+    scssPath: 'app/scss/**/*.scss',
+    jsPath: 'app/js/**/*.js'
 }
 
 //Sass task 
-function scssTask(){
-	return src(files.scssPath)
-	.pipe(sourcemaps.init())
-	.pipe(sass())
-	.pipe(postcss([ autoprefixer()]))
-	.pipe(cssnano())
-	.pipe(sourcemaps.write('.'))
-	.pipe(dest('dist')
-	);
+function scssTask() {
+    return src(files.scssPath)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist'));
 }
 
 
+// Browser Sync
+function browser_sync() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+}
 
 //JS task 
-
-function jsTask(){
+function jsTask() {
     return src(files.jsPath)
-    .pipe(concat('all.js'))
-    .pipe(terser())
-    .pipe(dest('dist')
+        .pipe(concat('all.js'))
+        .pipe(terser())
+        .pipe(dest('dist')
 
-    );
-
+        );
 }
 
-
-
 // Cachebusting task 
-
 const cbString = new Date().getTime();
-function cacheBustTask(){
-	return src(['index.html'])
-	.pipe(replace(/cb=\d+/g, 'cb=' +cbString))
-	.pipe(dest('.')
-	);
+
+function cacheBustTask() {
+    return src(['index.html'])
+        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+        .pipe(dest('.'));
 }
 
 
 // Watch task 
-
-
-function watchTask(){
-	watch([files.scssPath, files.jsPath],
-	  parallel(scssTask,jsTask));
+function watchTask() {
+    watch([files.scssPath, files.jsPath],
+        parallel(scssTask, jsTask));
 }
 
 
 // Default task 
-
 exports.default = series(
-    parallel(scssTask,jsTask),
+    parallel(scssTask, jsTask),
     cacheBustTask,
-    watchTask
-	);
-
+    parallel(watchTask, browser_sync)
+);
